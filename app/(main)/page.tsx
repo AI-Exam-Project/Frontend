c0aa9b0e3c8c3963f3de4dc8b7a3f265259272ad
Tasneem Esleem <tasneemesleem48@gmail.com>
2026-08-18 23:32:48 +0300

    Fix FeatureCard TypeScript

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,6 +1,6 @@
 // app/(main)/page.tsx
 import Hero from "@/components/Hero/Hero";
-import Features from "@/components/Features";
+import Features from "@/components/Features/Features";
 import ContactSection from "@/components/Contact/ContactSection";
 import SuccessStories from "@/components/SuccessStories/SuccessStories";
 
